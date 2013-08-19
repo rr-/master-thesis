142eaf06c0649a3395fc5b82335e6071fbd6db24
Marcin Kurczewski <mkurczew@gmail.com>
2013-08-19 11:12:11 +0200

    Dodana bibliografia

--- compile.sh
+++ compile.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 rm -f praca.pdf
-pdflatex thesis.tex && pdflatex thesis.tex && clear
+
+pdflatex thesis.tex && \
+bibtex thesis.aux && \
+pdflatex thesis.tex && \
+clear
+
 rm -f *.log
 rm -f *.aux
 rm -f *.toc
